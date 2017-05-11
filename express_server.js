431f5b20fd846dc1b29a3ae5c4b038447bf847ad
KJAVA19811815 <karunsingh1993@gmail.com>
2017-05-11 17:58:49 +0000

    server-side

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var app = express();
+var cookieParser = require('cookie-parser')
 var PORT = process.env.PORT || 8080;
 app.set("view engine", "ejs")
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 
 function generateRandomNumberString() {
   return Math.random().toString(36).substring(2,8);
@@ -16,18 +18,20 @@ var urlDatabase = {
 };
 
 app.get("/urls", (req,res) => {
-  let templateVars = {urls: urlDatabase};
+
+  let templateVars = {urlDatabase: urlDatabase, cookieUsername:req.cookies.username};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL : req.params.id };
+  let templateVars = { shortURL : req.params.id, urlDatabase: urlDatabase };
   res.render("urls_show", templateVars);
-  console.log(generateRandomNumberString());
+  // console.log(generateRandomNumberString());
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+   let templateVars = {urlDatabase: urlDatabase, cookieUsername:req.cookies.username};
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -44,6 +48,39 @@ app.get("/u/:shortURL", (req, res) => {
   var longURL = urlDatabase[req.params.shortURL];
   console.log(longURL);
   res.redirect(longURL);
+});
+//deleting url
+
+app.post("/urls/:id/delete", (req, res) => {
+  console.log(req.params.id);
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
+// updating url
+
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const newUrl = req.body.newUrl;
+  urlDatabase[id] = newUrl;
+  //let shortURL ;
+  //urlDatabase[shortURL] = newUrl
+  console.log(urlDatabase)
+  res.redirect("/urls/<%= id %>")
+});
+
+//cookie
+app.post("/login", (req, res) => {
+
+  res.cookie('username', req.body.username)
+
+  //res.render("../.._headers.ejs")
+  res.redirect("/urls")
+});
+
+app.post("/logout", (req, res) => {
+res.clearCookie('username');
+res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
